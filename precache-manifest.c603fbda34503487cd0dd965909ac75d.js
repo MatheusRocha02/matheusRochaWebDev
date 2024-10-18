14ab5f109c88a298d2acd5797b5e23cd563f4633
Matheus Rocha <matheusdcrocha15@gmail.com>
2024-10-18 10:05:04 -0300

    Updates

---
+++ precache-manifest.c603fbda34503487cd0dd965909ac75d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffebc796c05abf7dd03f83e2c77ec441",
+    "url": "/matheusRochaWebDeveloper/index.html"
+  },
+  {
+    "revision": "8161a958f55a10fe7ce2",
+    "url": "/matheusRochaWebDeveloper/static/css/main.37678f76.chunk.css"
+  },
+  {
+    "revision": "ef1b8b324cfb1661a058",
+    "url": "/matheusRochaWebDeveloper/static/js/2.80baec2a.chunk.js"
+  },
+  {
+    "revision": "8161a958f55a10fe7ce2",
+    "url": "/matheusRochaWebDeveloper/static/js/main.804f529b.chunk.js"
+  },
+  {
+    "revision": "c04a348272edcf8304bd",
+    "url": "/matheusRochaWebDeveloper/static/js/runtime~main.c74f778b.js"
+  },
+  {
+    "revision": "06a6fcb24d585508984652f6719758f6",
+    "url": "/matheusRochaWebDeveloper/static/media/CV Matheus Rocha.docx (1).06a6fcb2.pdf"
+  },
+  {
+    "revision": "6080d4d44520af1d0c8ab2a79face581",
+    "url": "/matheusRochaWebDeveloper/static/media/bernardo-cunha.6080d4d4.png"
+  },
+  {
+    "revision": "7ad9156b0cf3149a0829a522b893f14e",
+    "url": "/matheusRochaWebDeveloper/static/media/loginSystem-mockup.7ad9156b.png"
+  },
+  {
+    "revision": "1f006cb9813c3d0a55fb6ed51bad0552",
+    "url": "/matheusRochaWebDeveloper/static/media/matheus.1f006cb9.png"
+  },
+  {
+    "revision": "f28996611224cb28b9932f4754e3a03a",
+    "url": "/matheusRochaWebDeveloper/static/media/portifolio.f2899661.png"
+  }
+]);
